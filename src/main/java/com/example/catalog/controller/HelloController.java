6115438fa13171bda055bc316533ec36c0bfb42d
maias1907 <144439334+maias1907@users.noreply.github.com>
2025-01-01 18:48:42 +0200

    the project is finish

--- src/main/java/com/example/catalog/controller/HelloController.java
+++ src/main/java/com/example/catalog/controller/HelloController.java
@@ -1,3 +1,4 @@
+
 package com.example.catalog.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,10 @@ public class HelloController {
     @GetMapping("/")
     public String home() {
         return "Hello from API! There is no UI here...";
+    }
+    @GetMapping("/internal")
+    public  String internal(){
+        return "Internal Service";
     }
 
 }
